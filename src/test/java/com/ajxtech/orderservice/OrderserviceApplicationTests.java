aff7916b8926b6536175250956295e8ad25f3b5f
Rodrigo Silva <rodrigo.92.srb@gmail.com>
2026-06-30 14:34:16 -0300

    chore: first commit

---
+++ src/test/java/com/ajxtech/orderservice/OrderserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ajxtech.orderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
