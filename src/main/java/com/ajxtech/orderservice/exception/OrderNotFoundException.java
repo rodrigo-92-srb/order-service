238eb6c86dd257bce7694307695cd76237253bf7
Rodrigo Silva <rodrigo.92.srb@gmail.com>
2026-07-06 15:40:05 -0300

    feat: adjusts to use OrderNotFoundException

---
+++ src/main/java/com/ajxtech/orderservice/exception/OrderNotFoundException.java
@@ -0,0 +1,7 @@
+package com.ajxtech.orderservice.exception;
+
+public class OrderNotFoundException extends RuntimeException {
+    public OrderNotFoundException(String message) {
+        super(message);
+    }
+}
